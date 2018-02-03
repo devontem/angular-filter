f6b178f0c996a081f8865e40217ac9f7495d2330
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-03 14:19:47 -0800

    rendering config categories inside the categories componenet

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import categories from './categories.config';
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+	categories;
 
   constructor() { }
 
   ngOnInit() {
+		this.categories = categories;
+		console.log('h')
+		console.log(categories)
   }
 
 }
