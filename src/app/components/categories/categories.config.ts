82f9cb7b25616a41b3be5ac1c9148d5daf261f00
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-03 14:17:42 -0800

    added config files for category types and their thresholds

---
+++ src/app/components/categories/categories.config.ts
@@ -0,0 +1,123 @@
+const categories = [
+	{ 
+		title: "Year", 
+		key: "year",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	},
+	{ 
+		title: "Miles Per Gallon", 
+		key: "mpg",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	},
+	{ 
+		title: "Cylinders", 
+		key: "cyl",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	},
+	{ 
+		title: "Autonomous Cruise Control", 
+		key: "acc",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	},
+	{ 
+		title: "Digital Signal Processors", 
+		key: "dsp",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	},
+	{ 
+		title: "Weight (lbs)", 
+		key: "lbs",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	},
+	{ 
+		title: "Horsepower", 
+		key: "hp",
+		options: [ {
+			title: "< 10",
+			start: 0,
+			end: 10
+		}, {
+			title: "10 - 20",
+			start: 10,
+			end: 20
+		}, {
+			title: "20+",
+			start: 20,
+			end: null
+		} ]
+	}
+];
+
+export default categories;
