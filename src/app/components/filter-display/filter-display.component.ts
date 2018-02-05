c05ba79379cd87b3ab116bb970f2b45569b5d3d0
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-04 17:35:53 -0800

    added ability to remove items from filter list from the filter display section through the Output decorator

--- src/app/components/filter-display/filter-display.component.ts
+++ src/app/components/filter-display/filter-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 import Filter from './../../interfaces/filter.interface';
 
@@ -9,11 +9,20 @@ import Filter from './../../interfaces/filter.interface';
 })
 export class FilterDisplayComponent implements OnInit {
 	@Input() filters:Filter;
+	@Output() filterUpdateEvent:EventEmitter<object> = new EventEmitter<object>();
+
 	Objectkeys = Object.keys; // in order to iterate over object in template
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleClick(key, id){
+  	console.log('delete ', id);
+  	this.filters[key] = this.filters[key].filter((item) => item.id !== id);
+
+  	this.filterUpdateEvent.emit(this.filters);
   }
 
 }
