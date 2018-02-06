d2f79db51473339e45310b2bcb113696876dce22
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-06 12:27:43 -0800

    added spec tests for items-list component

--- src/app/components/items-list/items-list.component.spec.ts
+++ src/app/components/items-list/items-list.component.spec.ts
@@ -2,13 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemsListComponent } from './items-list.component';
 
+import { CarsService } from './../../services/cars.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
 describe('ItemsListComponent', () => {
   let component: ItemsListComponent;
   let fixture: ComponentFixture<ItemsListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemsListComponent ]
+      declarations: [ ItemsListComponent ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule
+      ],
+      providers: [CarsService]
     })
     .compileComponents();
   }));
@@ -21,5 +32,25 @@ describe('ItemsListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should applyFilters based on rawData and return all products with 21+ mpg', () => {
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    component.rawData = [{name: 'Chevy', mpg:20, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}, {name: 'Honda', mpg:9, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}];
+    component.filters = Object.assign({}, component.filters, {mpg: [{title: '21+', start: 21, end: null, id:5}]});
+    component.applyFilters();
+    expect(component.results.length).toEqual(0);
+  });
+
+  it('should applyFilters based on rawData and return all products with 10-20 mpg', () => {
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    component.rawData = [{name: 'Chevy', mpg:20, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}, {name: 'Honda', mpg:9, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}];
+    component.filters = Object.assign({}, component.filters, {mpg: [{title: '10-20', start: 10, end: 20, id:5}]});
+    component.applyFilters();
+    expect(component.results.length).toEqual(1);
   });
 });
