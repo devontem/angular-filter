58d66ba3e53f21233afd75b57d97d33fb430706a
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-06 12:25:44 -0800

    added tests for filter-display component

--- src/app/components/filter-display/filter-display.component.spec.ts
+++ src/app/components/filter-display/filter-display.component.spec.ts
@@ -22,4 +22,25 @@ describe('FilterDisplayComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should formatList from type Filter to array', () => {
+    fixture = TestBed.createComponent(FilterDisplayComponent);
+    component = fixture.componentInstance;
+
+    var filters = Object.assign({}, component.filters, {mpg: [{title: '21+', start: 21, end: null, id:5}]});
+    var filters2 = Object.assign({}, filters, {acc: [{title: '0-10', start: 0, end: 10, id:5}]})
+    expect(component.formatList(filters).length).toEqual(1);
+    expect(component.formatList(filters2).length).toEqual(2);
+    spyOn(component.filterUpdateEvent, 'emit');
+  });
+
+  it('should handleClick method should emit filterUpdateEvent', () => {
+    fixture = TestBed.createComponent(FilterDisplayComponent);
+    component = fixture.componentInstance;
+
+    spyOn(component.filterUpdateEvent, 'emit');
+    component.filters = Object.assign({}, component.filters, {mpg: [{title: '21+', start: 21, end: null, id:5}]});
+    component.handleClick('mpg', 5);
+    expect(component.filterUpdateEvent.emit).toHaveBeenCalled;
+  });
 });
