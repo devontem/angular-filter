4d50e319323c46e33db526ac7ae7c0cbd4ab84f9
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-05 16:32:32 -0800

    added httpModule and CarsService provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FilterDisplayComponent } from './components/filter-display/filter-display.component';
 import { ItemsListComponent } from './components/items-list/items-list.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { CarsService } from './services/cars.service';
 
 
 @NgModule({
@@ -18,9 +19,10 @@ import { CategoriesComponent } from './components/categories/categories.componen
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
