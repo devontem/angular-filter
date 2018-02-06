b02aa752eec5d5f5ad1e0a1721a867624be26694
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-06 12:26:30 -0800

    added providers, imports for CarsService spec config

--- src/app/services/cars.service.spec.ts
+++ src/app/services/cars.service.spec.ts
@@ -1,11 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CarsService } from './cars.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 describe('CarsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CarsService]
+      providers: [CarsService],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule
+      ],
     });
   });
 
