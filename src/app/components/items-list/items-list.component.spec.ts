44ad5ab0f6d9b415a7cc62ca9a17043ea3dd4e43
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-06 19:37:23 -0800

    updated and added additional tests for items-list component

--- src/app/components/items-list/items-list.component.spec.ts
+++ src/app/components/items-list/items-list.component.spec.ts
@@ -34,23 +34,41 @@ describe('ItemsListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should applyFilters based on rawData and return all products with 21+ mpg', () => {
+  it('applyFilters method should apply filter based on rawData and return all products with 21+ mpg', () => {
     fixture = TestBed.createComponent(ItemsListComponent);
     component = fixture.componentInstance;
 
-    component.rawData = [{name: 'Chevy', mpg:20, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}, {name: 'Honda', mpg:9, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}];
+    component.data = [{name: 'Chevy', mpg:20, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}, {name: 'Honda', mpg:9, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}];
     component.filters = Object.assign({}, component.filters, {mpg: [{title: '21+', start: 21, end: null, id:5}]});
     component.applyFilters();
     expect(component.results.length).toEqual(0);
   });
 
-  it('should applyFilters based on rawData and return all products with 10-20 mpg', () => {
+  it('applyFilters method should apply filter based on rawData and return all products with 10-20 mpg', () => {
     fixture = TestBed.createComponent(ItemsListComponent);
     component = fixture.componentInstance;
 
-    component.rawData = [{name: 'Chevy', mpg:20, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}, {name: 'Honda', mpg:9, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}];
+    component.data = [{name: 'Chevy', mpg:20, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}, {name: 'Honda', mpg:9, dsp: 10, acc:10, lbs:1000, hp:200, year:88, cyl:4}];
     component.filters = Object.assign({}, component.filters, {mpg: [{title: '10-20', start: 10, end: 20, id:5}]});
     component.applyFilters();
     expect(component.results.length).toEqual(1);
+  });
+
+  it('ngOnInit method should call getData', () => {
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    spyOn(component, 'getData');
+    component.ngOnInit();
+    expect(component.getData).toHaveBeenCalled;
+  });
+
+  it('ngOnChanges method should call applyFilters', () => {
+    fixture = TestBed.createComponent(ItemsListComponent);
+    component = fixture.componentInstance;
+
+    spyOn(component, 'applyFilters');
+    component.ngOnChanges({});
+    expect(component.applyFilters).toHaveBeenCalled;
   });
 });
