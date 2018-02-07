a1b03f53e4001533f0dc1d1099bc3ed9a5163a5f
Devonte MacGlashan <devontem@yahoo-inc.com>
2018-02-06 19:37:48 -0800

    updated and added tests for categories component

--- src/app/components/categories/categories.component.spec.ts
+++ src/app/components/categories/categories.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoriesComponent } from './categories.component';
+import { CarsService } from './../../services/cars.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 describe('CategoriesComponent', () => {
-  let component: CategoriesComponent;
-  let fixture: ComponentFixture<CategoriesComponent>;
+  let component:CategoriesComponent;
+  let fixture:ComponentFixture<CategoriesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CategoriesComponent ]
+      declarations: [ CategoriesComponent ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule
+      ],
+      providers: [CarsService]
     })
     .compileComponents();
   }));
@@ -21,5 +31,54 @@ describe('CategoriesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('handleChange method should emit filterUpdateEvent', () => {
+    fixture = TestBed.createComponent(CategoriesComponent);
+    component = fixture.componentInstance;
+
+    spyOn(component.filterUpdateEvent, 'emit');
+    var option = {title: '21+', start: 21, end: null, id:5};
+    component.filters = Object.assign({}, component.filters, {mpg: [option]});
+    component.handleChange('mpg', option);
+    expect(component.filterUpdateEvent.emit).toHaveBeenCalled;
+  });
+
+  it('handleChange should remove filter if present', () => {
+    fixture = TestBed.createComponent(CategoriesComponent);
+    component = fixture.componentInstance;
+
+    var option = {title: '21+', start: 21, end: null, id:5};
+    component.filters = Object.assign({}, component.filters, {mpg: [option]});
+    component.handleChange('mpg', option);
+    expect(component.filters.mpg.length).toEqual(0);
+  });
+
+  it('ngOnInit method should call getCategories', () => {
+    fixture = TestBed.createComponent(CategoriesComponent);
+    component = fixture.componentInstance;
+
+    spyOn(component, 'getCategories');
+    component.ngOnInit();
+    expect(component.getCategories).toHaveBeenCalled;
+  });
+
+  it('isChecked method should show false if filter is not applied', () => {
+    fixture = TestBed.createComponent(CategoriesComponent);
+    component = fixture.componentInstance;
+
+    var option = {title: '21+', start: 21, end: null, id:5};
+    var newOption = {title: '10-23', start: 10, end: 20, id:6};
+    component.filters = Object.assign({}, component.filters, {mpg: [option]});
+    expect(component.isChecked('mpg', newOption)).toEqual(false);
+  });
+
+  it('isChecked method should show true if filter is not applied', () => {
+    fixture = TestBed.createComponent(CategoriesComponent);
+    component = fixture.componentInstance;
+
+    var option = {title: '21+', start: 21, end: null, id:5};
+    component.filters = Object.assign({}, component.filters, {mpg: [option]});
+    expect(component.isChecked('mpg', option)).toEqual(true);
   });
 });
